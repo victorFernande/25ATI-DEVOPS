99c09c795aa1e0a8ff739bd45977a021a81e5eba
victorFernande <victorFernande@users.noreply.github.com>
2019-03-25 18:39:53 -0300

    Update alteraSSH.sh

--- alteraSSH.sh
+++ alteraSSH.sh
@@ -2,7 +2,7 @@
 read -r -p "Voce tem certeza que quer trocar a porta SSH? [Y/N]" response
 if [[ $response == "y" || $response == "Y" ]]
 then
-  sed -i 's/#Port 22/Port 2269/g' /etc/ssh/sshd_config
+  sed -i 's/Port 22/Port 2269/g' /etc/ssh/sshd_config
   echo "SSH Trocada para: 2269."
   sshPort=$( grep "Port" /etc/ssh/sshd_config | head -n 1 )
   echo "Confirmando porta:  $sshPort"
