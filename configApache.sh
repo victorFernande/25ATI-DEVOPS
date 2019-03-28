5be048e5fc591a0a217d8d37333a6f11cbe53dcf
victorFernande <victorFernande@users.noreply.github.com>
2019-03-27 21:39:10 -0300

    Update configApache.sh

--- configApache.sh
+++ configApache.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
+
+sudo touch /etc/apache2/sites-available/qa.conf
+sudo touch /etc/apache2/sites-available/dev.conf
+sudo touch /etc/apache2/sites-available/prod.conf
+
+
 sudo mkdir -p /var/www/html/qa.25ati.com
 sudo mkdir -p /var/www/html/prod.25ati.com
 sudo mkdir -p /var/www/html/dev.25ati.com
 
 ################################################################### 
+
+sudo echo "
+<VirtualHost *:80>
+    ServerName qa.25ati.com
+    ServerAdmin webmaster@25ati.com
+    DocumentRoot /var/www/html/qa.25ati.com
+    LogLevel warn
+</VirtualHost>
+" > /etc/apache2/sites-available/qa.conf
+
+
+sudo echo "
+<VirtualHost *:80>
+    ServerName dev.25ati.com
+    ServerAdmin webmaster@25ati.com
+    DocumentRoot /var/www/html/dev.25ati.com
+    LogLevel warn
+</VirtualHost>
+" > /etc/apache2/sites-available/dev.conf
+
+
+sudo echo "
+<VirtualHost *:80>
+    ServerName prod.25ati.com
+    ServerAdmin webmaster@25ati.com
+    DocumentRoot /var/www/html/prod.25ati.com
+    LogLevel warn
+</VirtualHost>
+" > /etc/apache2/sites-available/prod.conf
+
+
 
 
 sudo echo "
@@ -13,11 +50,11 @@ sudo echo "
 	</head>	" > /var/www/html/qa.25ati.com/index.html
 		echo "<body style=\"background-color:#DAF7A6;\"> " >> /var/www/html/qa.25ati.com/index.html
 		echo "<h1>QA Site</h1>
-		RM330586 - Daniel Asato
-		RM330459 - Danielle Silveira
-		RM330460 - Danilo Albuquerque Maciel
-		RM330461 - Tiago Rios Sartorato
-		RM331513 - Victor Fernandes 
+		RM330586 - Daniel Asato</br>
+		RM330459 - Danielle Silveira</br>
+		RM330460 - Danilo Albuquerque Maciel</br>
+		RM330461 - Tiago Rios Sartorato</br>
+		RM331513 - Victor Fernandes </br>
 	</body>
 </html>
 " >> /var/www/html/qa.25ati.com/index.html
@@ -29,11 +66,11 @@ echo "
 	</head> 	" > /var/www/html/dev.25ati.com/index.html
 	echo "<body style=\"background-color:##D6EAF8;\"> " >> /var/www/html/dev.25ati.com/index.html
 	echo "	<h1>DEV Site</h1>
-			RM330586 - Daniel Asato
-			RM330459 - Danielle Silveira
-			RM330460 - Danilo Albuquerque Maciel
-			RM330461 - Tiago Rios Sartorato
-			RM331513 - Victor Fernandes 
+		RM330586 - Daniel Asato</br>
+		RM330459 - Danielle Silveira</br>
+		RM330460 - Danilo Albuquerque Maciel</br>
+		RM330461 - Tiago Rios Sartorato</br>
+		RM331513 - Victor Fernandes </br>
 	</body>
 </html>
 " > /var/www/html/dev.25ati.com/index.html
@@ -45,73 +82,19 @@ echo "
 	</head> " > /var/www/html/prod.25ati.com/index.html
 	echo "<body style=\"background-color:##FDEBD0;\">  " >> /var/www/html/prod.25ati.com/index.html
 	echo "	<h1>PROD Site</h1>
-			RM330586 - Daniel Asato
-			RM330459 - Danielle Silveira
-			RM330460 - Danilo Albuquerque Maciel
-			RM330461 - Tiago Rios Sartorato
-			RM331513 - Victor Fernandes 
+		RM330586 - Daniel Asato</br>
+		RM330459 - Danielle Silveira</br>
+		RM330460 - Danilo Albuquerque Maciel</br>
+		RM330461 - Tiago Rios Sartorato</br>
+		RM331513 - Victor Fernandes </br>
 	</body>
 </html>
 " > /var/www/html/prod.25ati.com/index.html
 
 
-##################################################################
-
-sudo echo "" > /etc/apache2/sites-enabled/000-default.conf
-
-
-sudo echo "
-<VirtualHost *:80>
-    DocumentRoot /www/html/qa.25ati.com
-    ServerName qa.25ati.com " >> /etc/apache2/sites-available/000-default.conf
-    sudo echo "<Directory \"/www/html/qa.25ati.com\">" >> /etc/apache2/sites-available/000-default.conf
-    sudo echo "  Options Indexes FollowSymLinks
-		AllowOverride None
-		Require all granted
-    </Directory>
-</VirtualHost>
-
-" >> /etc/apache2/sites-enabled/000-default.conf
-
-
-
-sudo echo "
-<VirtualHost *:80>
-    DocumentRoot /www/html/dev.25ati.com
-    ServerName dev.25ati.com " >> /etc/apache2/sites-available/000-default.conf
-    sudo echo "<Directory \"/www/html/dev.25ati.com\">" >> /etc/apache2/sites-available/000-default.conf
-    sudo echo "  Options Indexes FollowSymLinks
-		AllowOverride None
-		Require all granted
-    </Directory>
-</VirtualHost>
-
-" >> /etc/apache2/sites-enabled/000-default.conf
-
-
-sudo echo "
-<VirtualHost *:80>
-    DocumentRoot /www/html/prod.25ati.com
-    ServerName prod.25ati.com " >> /etc/apache2/sites-available/000-default.conf
-    sudo echo "<Directory \"/www/html/prod.25ati.com\">" >> /etc/apache2/sites-available/000-default.conf
-    sudo echo "  Options Indexes FollowSymLinks
-		AllowOverride None
-		Require all granted
-    </Directory>
-</VirtualHost>
-
-" >> /etc/apache2/sites-enabled/000-default.conf
-
-a2ensite 000-default.conf
-
-cd /var/www
-sudo chmod 775 html
-sudo chgrp www-data html
-sudo chmod g+s html
-
-sudo chgrp -R www-data html
-find . -type f -exec chmod 664 {} \;
-find . -type d -exec chmod 775 {} \;
+a2ensite qa.conf
+a2ensite dev.conf
+a2ensite prod.conf
 
 service apache2 restart
 
